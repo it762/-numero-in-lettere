e6bfdd6c413f9d8542e4ee0d68c06db7eb9cede1
it762 <tirocinante@canalicchio.it>
2025-06-13 13:02:46 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-from flask import Flask, request, Response
+from flask import Flask, Response
 from num2words import num2words
 
 app = Flask(__name__)
 
-@app.route('/numero-in-lettere', methods=['GET'])
-def numero_in_lettere():
-    numero = request.args.get('numero')
+@app.route('/numero-in-lettere/<numero>', methods=['GET'])
+def numero_in_lettere(numero):
     try:
         numero = float(numero)
         parte_intera = int(numero)
         parte_decimale = int(round((numero - parte_intera) * 100))
         
         lettere = num2words(parte_intera, lang='it')
-        risultato = f"{lettere}/{parte_decimale:02d}"
+        risultato = f"{lettere} / {parte_decimale:02d}"
         
         return Response(risultato, mimetype='text/plain')
     except Exception as e:
