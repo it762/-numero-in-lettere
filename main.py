e5dd98d3bb56100f6c0b20c77409f252de6c08cd
it762 <tirocinante@canalicchio.it>
2025-06-13 11:45:21 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+from num2words import num2words
+
+app = Flask(__name__)
+
+@app.route('/numero-in-lettere', methods=['GET'])
+def numero_in_lettere():
+    numero = request.args.get('numero')
+    try:
+        numero = float(numero)
+        lettere = num2words(numero, lang='it')
+        return jsonify({"numero": numero, "lettere": lettere})
+    except Exception as e:
+        return jsonify({"errore": str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
