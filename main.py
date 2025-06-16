1ccd0354c65ed1ff572d0adfea5523f434205535
it762 <tirocinante@canalicchio.it>
2025-06-16 08:30:56 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def numero_in_lettere(numero):
         parte_decimale = int(round((numero - parte_intera) * 100))
         
         lettere = num2words(parte_intera, lang='it')
-        risultato = f"{lettere} / {parte_decimale:02d}"
+        risultato = f"{lettere}/{parte_decimale:02d}"
         
         return Response(risultato, mimetype='text/plain')
     except Exception as e:
