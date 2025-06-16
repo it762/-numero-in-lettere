d99ea590d80c29535462b7994e9913291db8917f
it762 <tirocinante@canalicchio.it>
2025-06-16 08:59:27 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ app = Flask(__name__)
 
 @app.route('/numero-in-lettere/<numero>', methods=['GET'])
 def numero_in_lettere(numero):
-    try:
-        numero = float(numero)
-        parte_intera = int(numero)
-        parte_decimale = int(round((numero - parte_intera) * 100))
-        
-        lettere = num2words(parte_intera, lang='it')
-        risultato = {
-            "valore": f"{lettere} / {parte_decimale:02d}"
-        }
-        return jsonify(risultato)
-    except Exception as e:
-        return jsonify({"errore": str(e)}), 400
+    try:
+        numero = float(numero)
+        parte_intera = int(numero)
+        parte_decimale = int(round((numero - parte_intera) * 100))
+        
+        lettere = num2words(parte_intera, lang='it')
+        risultato = {
+            "valore": f"{lettere} / {parte_decimale:02d}"
+        }
+        return jsonify(risultato)
+    except Exception as e:
+        return jsonify({"errore": str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
